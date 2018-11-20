cfee8f9b5a6ad06d3a3d8e2c86dc8fc92c660350
Chanuka Weerasinghe <chanuka.weer@gmail.com>
2018-11-20 14:56:51 +0000

    TEST
    TEST

---
+++ publish/wwwroot/AngularJS/controller.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('App_Monitor')
+        .controller('ApiLogController', ApiLogController);
+
+    function ApiLogController($scope, $http, $interval) {
+
+       //$http({
+       //     method: 'GET',
+       //     url: '/apilog/getUpdates'
+       // }).then(function successCallback(response) {
+       //     // this callback will be called asynchronously
+       //     // when the response is available
+       //     $scope.api = response.data;
+
+       // }, function errorCallback(response) {
+       //     // called asynchronously if an error occurs
+       //     // or server returns response with an error status.
+       //     });
+
+        var data = function () {
+            $.ajax({
+                url: '/apilog/getUpdates',
+                type: "GET",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: function (response) {
+                    console.log("success");
+                    $scope.api = response;
+                    console.log(response);
+                },
+                error: function (response) {
+                    console.log("failed");
+                }
+            })
+        };
+
+        $interval(data, 1000); 
+        //data(); 
+    }
+})();
+
